ef063ed7222c172b6ec213c943646b7e39a6dc26
Surya0191 <ssuryateja804@gmail.com>
2024-06-02 11:04:55 +0530

    removed references of temp-project

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'temp-project'`, () => {
+  it(`should have as title 'personal-portfolio'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('temp-project');
+    expect(app.title).toEqual('personal-portfolio');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('temp-project app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('personal-portfolio app is running!');
   });
 });
